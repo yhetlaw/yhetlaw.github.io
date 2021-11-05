e12b0d7f448b9e83959b47be50d028c06b3710cc
yhetlaw <jandresantos14@gmail.com>
2021-11-05 17:52:25 +0000

    update

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -5,27 +5,20 @@ import Search from './Search';
 const GetStarted = () => {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
-    console.log('effect');
     axios.get('https://restcountries.com/v2/all').then((response) => {
-      console.log('countries promise fulfilled');
       setCountries(response.data);
     });
   }, []);
-  console.log('render', countries.length, 'countries');
-  console.log('the countries are ', countries);
 
   const [weather, setWeather] = useState([]);
   const [capital, setCapital] = useState(null);
-  console.log('The capital in App.js is', capital);
 
   useEffect(() => {
-    console.log('effect');
     if (!capital) setCapital('Lisbon');
     const api_key = process.env.REACT_APP_API_KEY;
     axios
       .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
       .then((response) => {
-        console.log('weather promise fulfilled');
         setWeather(response.data);
       })
       .catch((error) => {
