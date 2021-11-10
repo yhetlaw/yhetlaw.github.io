d53daf3402e6b3e6a031ff3335f1341c9b5dfd2a
yhetlaw <jandresantos14@gmail.com>
2021-11-10 14:50:28 +0000

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './app';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
